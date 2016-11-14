b0cd92ec636b3180ec92ea5dbb6190e657e26dca
151473E <151473E@mymail.nyp.edu.sg>
2016-11-14 16:10:59 +0800

    practical5

---
+++ src/demo/Book.java
@@ -0,0 +1,18 @@
+package demo;
+
+/**
+ * Created by 151473E on 11/14/2016.
+ */
+public class Book {
+    String title;
+    String description;
+    String author;
+    int year;
+
+    public Book(){
+        this.title="Java Made easy";
+        this.description="A book about how to learn Java in painless manner";
+        this.author="Java Duke";
+        this.year=2016;
+    }
+}
